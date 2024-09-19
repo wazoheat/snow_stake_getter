a09476ca86efe9dabf27ff57401dcce3d0bd27e5
Wazo Heat <wazoheat@gmail.com>
2024-09-19 15:21:52 -0600

    First version working from crontab. To run from cron at top of the hour, use the following line:
    0 * * * * cd path/to/snow_stakes && ./cron_caller.sh >& log

---
+++ cron_caller.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/Users/kavulich/.conda/envs/main/bin:/opt/miniforge3/condabin
+cd /Volumes/d1/kavulich/snow_stakes
+python3 snowstake_getter.py -v
--- snowstake_getter.py
+++ snowstake_getter.py
@@ -1,15 +1,37 @@
-import pathlib
-import requests
+import argparse
+import datetime as dt
+import pathlib as pl
+import urllib.request
 import yaml
 
-from bs4 import BeautifulSoup
 
-conf = yaml.safe_load(pathlib.Path('stakes.yml').read_text())
+def save_images(verbose):
+    nowstr=dt.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+    conf = yaml.safe_load(pl.Path('stakes.yml').read_text())
 
-for entry in conf:
-    r = requests.get(conf[entry]["url"])
-    html_doc=r.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    # Trick opensnow into thinking we're a browser, thanks Tonny Xu on StackOverflow!
+    # https://stackoverflow.com/questions/2364593/urlretrieve-and-user-agent-python/43501438#43501438 
+    proxy = urllib.request.ProxyHandler({})
+    opener = urllib.request.build_opener(proxy)
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30')]
+    urllib.request.install_opener(opener)
 
-    print(soup.prettify())
 
+    for entry in conf:
+        pl.Path(entry).mkdir(parents=True, exist_ok=True)
+        if verbose:
+            print(f"getting {entry}/{nowstr}.jpg from {conf[entry]['url']}")
+        result = urllib.request.urlretrieve(conf[entry]["url"], f"{entry}/{nowstr}.jpg")
+        if verbose:
+            print(f"{result=}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                     description="Script for saving snowstake images locally")
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Script will be run with more verbose output')
+
+    pargs = parser.parse_args()
+    save_images(pargs.verbose)
