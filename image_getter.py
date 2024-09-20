1be77cf2ed5d4e000ebc55c64880082eab83c392
Wazo Heat <wazoheat@gmail.com>
2024-09-20 12:24:42 -0600

    Add datestring to url to avoid website feeding us old/cached images. Also starting the framework for duplicate-checking code.

--- image_getter.py
+++ image_getter.py
@@ -5,8 +5,9 @@ import urllib.request
 import yaml
 
 
-def save_images(verbose):
+def save_images(verbose,keep):
     nowstr=dt.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
+    nowstrm=dt.datetime.utcnow().strftime('%Y%m%d%H%M')
     conf = yaml.safe_load(pl.Path('stakes.yml').read_text())
 
     # Trick opensnow into thinking we're a browser, thanks Tonny Xu on StackOverflow!
@@ -16,14 +17,19 @@ def save_images(verbose):
     opener.addheaders = [('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30')]
     urllib.request.install_opener(opener)
 
-
     for entry in conf:
         pl.Path(entry).mkdir(parents=True, exist_ok=True)
+        # If you don't include this "?datestring", the server feeds you an old/cached image
+        url=f"{conf[entry]['url']}?{nowstrm}"
+        localname = f"{entry}/{nowstr}.jpg"
         if verbose:
-            print(f"getting {entry}/{nowstr}.jpg from {conf[entry]['url']}")
-        _,result = urllib.request.urlretrieve(conf[entry]["url"], f"{entry}/{nowstr}.jpg")
+            print(f"getting {localname} from {url}")
+        _,result = urllib.request.urlretrieve(url, localname)
         if verbose:
             print(f"{result.as_string(unixfrom=True)=}")
+        if not keep:
+            if verbose:
+                print("Checking if latest image is new or not")
 
 
 if __name__ == "__main__":
@@ -32,6 +38,9 @@ if __name__ == "__main__":
 
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Script will be run with more verbose output')
+    parser.add_argument('-k', '--keep_duplicates', action='store_true',
+                        help='Script will be run with more verbose output')
+
 
     pargs = parser.parse_args()
-    save_images(pargs.verbose)
+    save_images(pargs.verbose,pargs.keep_duplicates)
